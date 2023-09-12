6e30e76e8af98a4eac1ee24752d53ebbf888ace9
DanielAugustoSant <80176793+DanielAugustoFreire@users.noreply.github.com>
2023-09-12 16:47:49 -0300

    Iso

--- computador.c
+++ computador.c
@@ -1,41 +1,83 @@
 #include <stdio.h>
+#include <windows.h>
+
 #define TAM 12
 
-void algoritmo(int posicao[2], int labi[12][12])
+/*
+    2
+1       3
+    4
+*/
+
+
+int direita(int posicao[2], const int labi[12][12], int direita[2]) {
+    int l=posicao[0],c=posicao[1];
+    int iso = 0; // Inicializa com valor padrão (neste caso, nenhum movimento)
+
+    if(labi[l-1][c] == 1 && labi[l+1][c] == 1 && labi[l][c+1] == 0)
+        iso =  1;
+    else if(labi[l][c-1] == 1 && labi[l][c] == 1 && labi[l+1][c] == 0)
+        iso =  2;
+    else if(labi[l-1][c] == 1 && labi[l+1][c] == 1 && labi[l][c-1] == 0)
+        iso =  3;
+    else if(labi[l][c-1] == 1 && labi[l][c] == 1 && labi[l-1][c] == 0)
+        iso =  4;
+
+    // Atualiza a posição do movimento a ser feito
+    if(iso == 1) {
+        direita[0] = l+1;
+        direita[1] = c;
+    }
+    else if(iso == 2) {
+        direita[0] = l;
+        direita[1] = c-1;
+    }
+    else if(iso == 3) {
+        direita[0] = l-1;
+        direita[1] = c;
+    }
+    else if(iso == 4) {
+        direita[0] = l;
+        direita[1] = c+1;
+    }
+
+    return iso;
+}
+
+int algoritmo(int posicao[2], const int labi[12][12], int direita[2])
 {
     int l=posicao[0],c=posicao[1];
+    int i, k;
 
-    if(labi[l+1][c] == 1 && labi[l][c+1] == 0)
+    for(i=-1;i<2;i++)
     {
-        c++;
+        for(k=-1;k<2;k++)
+        {
+            if(posicao[0] == direita[0] + i && posicao[1] == direita[0] + k)
+        }
     }
-    else if(labi[l][c+1] == 1 && labi[l-1][c] == 0)
-    {
-        l--;
-    }    
-    else if(labi[l][c] && labi[l-1][c])
-    {
 
-    }    
-    else if(labi[l][c] && labi[l-1][c])
-    {
 
-    }    
-    else if(labi[l][c] && labi[l][c])
-    {
 
-    }
+
 
 
     posicao[0] = l;
     posicao[1] = c;
+
+    if(posicao[0] == 4 && posicao[1] ==11)
+        return 10;
+    else 
+        return 1;
 }
 
-void printar(int lab[12][12], int posicao[2])
+void printar(const int lab[12][12], int posicao[2])
 {
-    for (int l = 0; l < TAM; l++)
+	int l, c;
+	
+    for (l = 0; l < TAM; l++)
     {
-        for (int c = 0; c < TAM; c++)
+        for (c = 0; c < TAM; c++)
         {
             if (lab[l][c] == 1)
                 printf("1 ");
@@ -51,7 +93,7 @@ void printar(int lab[12][12], int posicao[2])
 
 int main()
 {
-    int labirinto[12][12] = {
+    const int labirinto[12][12] = {
         1,1,1,1,1,1,1,1,1,1,1,1,
         1,0,0,0,1,0,0,0,0,0,0,1,
         0,0,1,0,1,0,1,1,1,1,0,1,
@@ -66,8 +108,18 @@ int main()
         1,1,1,1,1,1,1,1,1,1,1,1
     };
     int pos[2] = {2,0};
+    int i=0, isso;
+    int direitinha[2];
 
-    printar(labirinto, *pos);
+    isso = direita(pos, labirinto, direitinha);
+
+    while(i!=10)
+    {
+        printar(labirinto, pos);
+        i = algoritmo(pos, labirinto, direitinha);
+        Sleep(1000);
+        system("cls");
+    }
 
     return 0;
 }
